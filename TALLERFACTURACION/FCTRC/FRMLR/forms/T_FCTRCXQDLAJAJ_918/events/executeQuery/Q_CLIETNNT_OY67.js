5a27f748153c9202f4544b6e4a0137522bb0a776
Erick Cordova G <ericksitocg@hotmail.com>
2021-10-16 18:54:49 -0500

    Add:Grid to Formulario

---
+++ TALLERFACTURACION/FCTRC/FRMLR/forms/T_FCTRCXQDLAJAJ_918/events/executeQuery/Q_CLIETNNT_OY67.js
@@ -0,0 +1,6 @@
+
+//ClienteQuery Entity: Cliente
+task.executeQuery.Q_CLIETNNT_OY67 = function(executeQueryEventArgs){
+    executeQueryEventArgs.commons.execServer = true;
+    //executeQueryEventArgs.commons.serverParameters.Cliente = true;
+};
